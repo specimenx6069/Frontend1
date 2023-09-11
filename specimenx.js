d9ec33a00c03583266610f352d58b2042bba48f0
specimenx6069 <33862671+specimenx6069@users.noreply.github.com>
2023-09-11 10:30:20 +0100

    Add files via upload

---
+++ specimenx.js
@@ -0,0 +1,16 @@
+const updateUTCTime = () => {
+    const daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    const currentDate = new Date()
+    const currentDayOfTheWeek = daysOfTheWeek[currentDate.getUTCDay()]
+    document.querySelector("[data-testid='currentDayOfTheWeek']").textContent = `Current day of the week: ${currentDayOfTheWeek}`
+
+    const currentUTCTime = currentDate.toUTCString()
+    const currentUTCTimeInMilliSeconds = Date.parse(currentUTCTime)
+    document.querySelector("[data-testid='currentUTCTime']").textContent = `Current UTC time: ${currentUTCTimeInMilliSeconds}`
+    console.log(currentDayOfTheWeek);
+    console.log(currentUTCTimeInMilliSeconds);
+}
+
+setInterval(updateUTCTime,1000)
+
+updateUTCTime()
